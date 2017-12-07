9950f75daff53a9ebb2ee4cb55f229253c2ba6b6
p1510494 <kevin.sadecki@etu.univ-lyon1.fr>
2017-12-07 21:31:23 +0100

    Intégration de bootstrap + début de la mise en place de la map

--- js/app.js
+++ js/app.js
@@ -2,9 +2,17 @@
 var app = new Vue({
   el:"#app",
   data: {
-
+    map: 0,
   },
   methods: {
-    
+    log : function(msg){
+      console.log(msg);
+    },
+    initMap : function(){
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 0, lng: 0},
+        zoom: 8
+      });
+    }
   }
 });
--- js/components.js
+++ js/components.js
@@ -1,5 +1,10 @@
-/* Fichier des components */
+/* Fichier des components
 Vue.component("nomducomponent", {
   props: [],
   template: ""
+});
+*/
+Vue.component("carte", {
+  props: [],
+  template: "<div id='map' class='panel-body' style='height:100%;width:100%'></div>"
 });
