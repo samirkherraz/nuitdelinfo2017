f682198762cb4fb16e6aacb7e04a23decc1d4a51
p1510494 <kevin.sadecki@etu.univ-lyon1.fr>
2017-12-07 22:08:49 +0100

    Géolocalisation au démarrage de l'appli + centrage de la carte sur notre position

--- js/app.js
+++ js/app.js
@@ -13,6 +13,27 @@ var app = new Vue({
         center: {lat: 0, lng: 0},
         zoom: 8
       });
+
+      let infoWindow = new google.maps.InfoWindow({map: map});
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          let pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
     }
   }
 });
